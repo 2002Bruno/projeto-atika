b1286cc1c713da2c7de21979021e6d36ac7d9930
2002Bruno <brunno.rodrigues0706@gmail.com>
2023-04-15 20:15:41 -0300

    subindo projeto para o repositório

---
+++ target/classes/data.sql
@@ -0,0 +1,13 @@
+
+-- tbl_categoria --
+INSERT INTO public.tbl_categoria (id, nome) VALUES (1, 'Sênior');
+INSERT INTO public.tbl_categoria (id, nome) VALUES (2, 'Pleno');
+INSERT INTO public.tbl_categoria (id, nome) VALUES (3, 'Junior');
+INSERT INTO public.tbl_categoria (id, nome) VALUES (4, 'Estagiário');
+
+-- tbl_pessoa --
+INSERT INTO public.tbl_pessoa (id, bairro, cep, cidade, complemento, estado, logradouro, numero, nome, status) VALUES (1, 'P norte', '72241314', 'Brasília', 'casa com o portão branco', 'DF', 'QNP 17 conjunto h', '3', 'Dalthon Mike Rodrigues Dias', 'ATIVO');
+INSERT INTO public.tbl_pessoa (id, bairro, cep, cidade, complemento, estado, logradouro, numero, nome, status) VALUES (2, 'Lagoa Azul', '72241314', 'Natal', 'casa com o portão branco', 'RN', 'Rua Silvino Alves de Lima', '22', 'Zeormora Mike chaves', 'ATIVO');
+INSERT INTO public.tbl_pessoa (id, bairro, cep, cidade, complemento, estado, logradouro, numero, nome, status) VALUES (3, 'limoeiro', '59139-560', 'São Paulo ', '', 'SP', 'Rua das aves', '15', 'Felipe Lemos', 'ATIVO');
+
+
